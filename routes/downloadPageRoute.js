503aa79a15c639a23bd61b64ea662353e393b2d2
0C1earHeaded0 <159591431+0C1earHeaded0@users.noreply.github.com>
2024-09-19 12:32:01 +1100

    clean

--- routes/downloadPageRoute.js
+++ routes/downloadPageRoute.js
@@ -1,15 +1,11 @@
-// import isAuth from "../models/isAuth.js";
-
 const getDownloadPage = async (req, res) => {
     if (req.user) {
         res.render('downloadPage');
-        // res.redirect('http://localhost:3000/download');
     }
     else
         return res
             .status(401)
             .json({ message: 'Not authorized' });
-    
 }
 
 const download = async (app) => {
